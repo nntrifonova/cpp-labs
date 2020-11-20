46486337ab2cfc9dea604236360142127d7ab5c2
nntrifonova <72243446+nntrifonova@users.noreply.github.com>
2020-11-20 14:59:29 +0700

    rna

---
+++ rna/Project2/rna.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#ifndef RNA_RNA_H
+#define RNA_RNA_H
+
+enum Nucleotide { A, G, C, T };
+class rna
+{
+private:
+	int nucl_num;
+	int  capacity;
+	size_t* rna_arr;
+	class reference {
+	private:
+		int num;
+		rna* rna_array;
+	public:
+		reference(int n, rna* rna_ar) {
+			num = n;
+			rna_array = rna_ar;
+		}
+		void operator=(Nucleotide nucl) {
+
+		};
+		operator Nucleotide() const {
+			return (Nucleotide)(*rna_array).get_nucl(num);
+		}
+	};
+	class reference_const {
+	private:
+		int ind;
+		const rna* rna_array;
+	public:
+		reference_const(int index, rna* rna_arr) {
+			ind = index;
+			rna_array = rna_arr;
+		}
+		operator Nucleotide() const {
+			return (Nucleotide)(*rna_array).get_nucl(ind);
+		}
+
+	};
+public:
+	rna();
+	int count_capacity(int numb);
+	void insert(Nucleotide nucl);
+	rna(int numb);
+	rna(Nucleotide nucl, int nucl_num);
+	virtual ~rna();
+	Nucleotide get_nucl(int ind);
+
+	friend rna operator+(const rna &rna1, const rna &rna2);
+	friend bool operator == (const rna& nucl_1, const rna& nucl_2);
+	friend bool operator != (const rna& nucl_1, const rna& nucl_2);
+	friend rna operator ! (const rna &rna1);
+	friend bool is_complimentary(const rna& nucleotide1, const rna& nucleotide2);
+	reference operator [](int index);
+};
+#endif //RNA_RNA_H
